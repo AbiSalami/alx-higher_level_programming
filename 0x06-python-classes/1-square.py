6361619efa6454358ec140b2996e8e2d99130df6
AbiSalami <bimwealth215@gmail.com>
2024-01-23 11:20:02 -0800

    square size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 #1-square.py
+"""
+Module: 0-square
+"""
 
 class Square:
+    """
+    Defines a square with a private instance attribute: size
+    """
+
     def __init__(self, size):
+        """
+        Initializes the Square instanv=ce with a given size.
+        """
+
         self.__size = size
