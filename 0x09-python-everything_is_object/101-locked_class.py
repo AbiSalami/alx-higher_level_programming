3b66ca9e5cef60a685cd97a11939dc4208203625
AbiSalami <bimwealth215@gmail.com>
2024-01-30 23:40:53 +0100

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,4 +8,11 @@ class LockedClass:
     for anything but attributes called 'first_name'.
     """
 
-    __slots__ = ["first_name"] 
+    __slots__ = ["first_name"]
+
+     def __setattr__(self, name, value):
+         """Prevents dynamic creation of new attributes except 'first_name'."""
+         if name != 'first_name':
+             raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                .format(name))
+         super().__setattr__(name, value)
