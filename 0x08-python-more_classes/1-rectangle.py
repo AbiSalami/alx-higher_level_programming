909d85a716be6a1b1f3f94cabae3dfc3733fc9f5
AbiSalami <bimwealth215@gmail.com>
2024-02-01 02:43:56 +0100

    definition of rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+# 1-rectangle.py
+"""Defines a Rectangle class."""
+
+
 
 class Rectangle:
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
