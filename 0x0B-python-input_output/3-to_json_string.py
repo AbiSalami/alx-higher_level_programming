453f68c79ff5bef6fa35ab024849dadc6353c651
AbiSalami <bimwealth215@gmail.com>
2024-02-28 08:48:15 +0100

    Json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-#3-to_json_string.py
+"""Defines a string-to-JSON function."""
 import json
 
-def to_json_string(my_obj):
-    """
-    Convert a Python object to its JSON representation as a string.
-    
-    Args:
-        my_obj (obj): A Python object.
-        
-    Returns:
-        str: The JSON representation of the object.
-    """
-    return json.dumps(my_obj)
 
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
